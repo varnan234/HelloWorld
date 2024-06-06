b76d73054ba30db84ccf34ebbbc07b95064f8425
Varnan Goenka <varnan234@gmail.com>
2024-06-06 14:03:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# HelloWorld
+
+A description about what this project does
+
+## Resources
+List any resources you used to solva/enhance this project
+I will allow 2 GPT helps (I need the prompt)
---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.varnan","c":"HelloWorld","l":"generateGreeting(String, String)","u":"generateGreeting(java.lang.String,java.lang.String)"},{"p":"com.varnan","c":"HelloWorld","l":"HelloWorld()","u":"%3Cinit%3E()"},{"p":"com.varnan","c":"HelloWorld","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
